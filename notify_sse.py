936954c85573af3cf6fd77c337f72cf4f1866473
Kajigga Dev <kajigga@gmail.com>
2019-03-12 13:07:19 -0600

    lkjlkj

--- notify_sse.py
+++ notify_sse.py
@@ -7,7 +7,7 @@ from sseapiclient.tornado import SyncClient
 
 print(os.environ)
 
-with SyncClient.connect('https://sse.kajigga.com', 'root', sys.argv[1], ssl_validate_cert=False) as client:
-    print(client.api.cmd.route_cmd(cmd='local',
+client = SyncClient.connect('https://sse.kajigga.com', 'root', sys.argv[1], ssl_validate_cert=False)
+print(client.api.cmd.route_cmd(cmd='local',
                                job_uuid='71ccbd4c-5377-11e6-ba39-080027a7289c',
                                tgt_uuid='c19f6dd1-efc6-4164-b40e-0bf0d6549d62').ret)
