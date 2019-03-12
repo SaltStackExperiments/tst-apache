826178c8d5d40b7c14b81d33ba432747d1df21ef
Kajigga Dev <kajigga@gmail.com>
2019-03-12 09:59:29 -0600

    Add job_uuid and tgt_uuid to script

--- notify_sse.sh
+++ notify_sse.sh
@@ -7,4 +7,10 @@ curl -k -c $HOME/eAPICookie.txt -u root:$SSEAPI_PASS 'https://sse.kajigga.com/ve
 export TOKEN=$(grep -w '_xsrf' $HOME/eAPICookie.txt | cut -f7)
 
 # make the api call
-curl -k -u root:$SSEAPI_PASS -b $HOME/eAPICookie.txt -H "X-Xsrftoken: '$TOKEN'" -X POST https://sse.kajigga.com/rpc -d'{"resource": "auth", "method": "get_all_users", "kwarg": { "config_name": "internal" }}'
+curl -k -u root:$SSEAPI_PASS -b $HOME/eAPICookie.txt  \
+    -H 'X-Xsrftoken: '$(grep -w '_xsrf' $HOME/eAPICookie.txt | cut -f7)'' \
+    -X POST https://sse.kajigga.com/rpc \
+    -d'{"resource": "cmd", "method": "route_cmd", "kwarg": { "cmd": "local",\
+        "job_uuid": "71ccbd4c-5377-11e6-ba39-080027a7289c",\
+        "tgt_uuid": "c19f6dd1-efc6-4164-b40e-0bf0d6549d62" }}'
+
