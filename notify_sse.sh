190ad36ef093519a14c86cbd1932b074ab62236a
Kajigga Dev <kajigga@gmail.com>
2019-03-12 12:44:25 -0600

    now it's python

--- notify_sse.sh
+++ notify_sse.sh
@@ -1,18 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env python
 
-export SSEAPI_PASS=$1
+import sys
+from sseapiclient.tornado import SyncClient
+client = SyncClient.connect('https://sse.kajigga.com', 'root', sys.argv[1], ssl_validate_cert=False)
 
-# do the initial authentication
-curl -k -c $HOME/eAPICookie.txt -u root:$SSEAPI_PASS 'https://sse.kajigga.com/version' >/dev/null
-
-# gather the token
-export TOKEN=$(grep -w '_xsrf' $HOME/eAPICookie.txt | cut -f7)
-
-# make the api call
-curl -k -u root:$SSEAPI_PASS -b $HOME/eAPICookie.txt  \
-    -H 'X-Xsrftoken: '$(grep -w '_xsrf' $HOME/eAPICookie.txt | cut -f7)'' \
-    -X POST https://sse.kajigga.com/rpc \
-    -d'{"resource": "cmd", "method": "route_cmd", "kwarg": { "cmd": "local",\
-        "job_uuid": "71ccbd4c-5377-11e6-ba39-080027a7289c",\
-        "tgt_uuid": "c19f6dd1-efc6-4164-b40e-0bf0d6549d62" }}'
-
+print(client.cmd.route_cmd(cmd='local',
+                           job_uuid='71ccbd4c-5377-11e6-ba39-080027a7289c',
+                           tgt_uuid='c19f6dd1-efc6-4164-b40e-0bf0d6549d62'))
