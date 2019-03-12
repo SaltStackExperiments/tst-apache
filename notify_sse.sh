04d18922590db194b4a69a0082a5505a38612255
Kajigga Dev <kajigga@gmail.com>
2019-03-12 12:02:03 -0600

    Test script

--- notify_sse.sh
+++ notify_sse.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+
+whoami
+
+salt-call event.send build/successful
+
 # do the initial authentication
-curl -k -c $HOME/eAPICookie.txt -u root:$SSEAPI_PASS 'https://sse.kajigga.com/version' >/dev/null
+# curl -k -c $HOME/eAPICookie.txt -u root:$SSEAPI_PASS 'https://sse.kajigga.com/version' >/dev/null
 
-# gather the token
-export TOKEN=$(grep -w '_xsrf' $HOME/eAPICookie.txt | cut -f7)
+# # gather the token
+# export TOKEN=$(grep -w '_xsrf' $HOME/eAPICookie.txt | cut -f7)
 
-# make the api call
-curl -k -u root:$SSEAPI_PASS -b $HOME/eAPICookie.txt  \
-    -H 'X-Xsrftoken: '$(grep -w '_xsrf' $HOME/eAPICookie.txt | cut -f7)'' \
-    -X POST https://sse.kajigga.com/rpc \
-    -d'{"resource": "cmd", "method": "route_cmd", "kwarg": { "cmd": "local",\
-        "job_uuid": "71ccbd4c-5377-11e6-ba39-080027a7289c",\
-        "tgt_uuid": "c19f6dd1-efc6-4164-b40e-0bf0d6549d62" }}'
+# # make the api call
+# curl -k -u root:$SSEAPI_PASS -b $HOME/eAPICookie.txt  \
+#     -H 'X-Xsrftoken: '$(grep -w '_xsrf' $HOME/eAPICookie.txt | cut -f7)'' \
+#     -X POST https://sse.kajigga.com/rpc \
+#     -d'{"resource": "cmd", "method": "route_cmd", "kwarg": { "cmd": "local",\
+#         "job_uuid": "71ccbd4c-5377-11e6-ba39-080027a7289c",\
+#         "tgt_uuid": "c19f6dd1-efc6-4164-b40e-0bf0d6549d62" }}'
 
